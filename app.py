9560cdeba9e0c7ca5e61ac9010bbc77410bc536e
atiqrehman63 <rehmanrao63@gmail.com>
2026-03-22 01:05:26 +0000

    Added environment variables to microservice app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -10,7 +11,10 @@ def home():
 def info():
     return {
         "app": "flask-microservice-app",
-        "status": "running"
+        "status": "running",
+        "environment": os.getenv("APP_ENV"),
+        "redis_host": os.getenv("REDIS_HOST"),
+        "db_host": os.getenv("DB_HOST")
     }
 
 if __name__ == "__main__":
